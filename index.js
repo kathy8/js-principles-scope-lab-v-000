dd65f746000e5b28973ab32027e4bb568ecceedd
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-03-27 21:58:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 var customerName = 'bob;'
 
 function upperCaseCustomerName() {customerName.toUpperCase();
-  
+
 }
+function setBestCustomer() {
+   bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+ const leastFavoriteCustomer = "The Hamburglar";
+
+ function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = "Dan"
+ }
